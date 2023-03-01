5818cd78574c335a33f530587f529d508b5d0515
Jacky Ngo <jacky.ngo96@gmail.com>
2023-03-01 14:29:03 -0500

    Retrieve appointment data using Promise.all

--- src/components/Application.js
+++ src/components/Application.js
@@ -10,6 +10,7 @@ import Appointment from "components/Appointment";
 
 import axios from 'axios';
 
+import {getAppointmentsForDay} from "helpers/selectors"
 
 // const appointments = {
 //   "1": {
@@ -52,24 +53,29 @@ import axios from 'axios';
 
 
 export default function Application(props) {
+  
   const [state, setState] = useState({
     day: "Monday",
     days: [],
     appointments: {}
   });
+  
   const setDay = day => setState({ ...state, day });
   
-  const setDays = days => setState(prev => ({ ...prev, days }));
-    
+  const dailyAppointments = getAppointmentsForDay(state ,state.day);
   
   useEffect(() =>{ 
-    const url =  "http://localhost:8001/api/days"
-    axios.get(url).then((response) => {
-      setDays(response.data)
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments')
+    ]).then((all) => {
+      console.log(all)
+      setState(prev => ({...prev, days:all[0].data, appointments:all[1].data}))
     });
   },[]);
   
-  const mappedAppointments = Object.values(state.appointments).map((appointment) => {
+  
+  const mappedAppointments = dailyAppointments.map((appointment) => {
     return <Appointment key={appointment.id} {...appointment}/>
   }); 
  return (
